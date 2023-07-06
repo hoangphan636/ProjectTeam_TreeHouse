30cda0c3e301534b5c31346a3eb92f89593a9df5
An <tannbn01@gmail.com>
2023-07-06 09:07:54 +0700

    .

--- BusinessObject/Tokens.cs
+++ BusinessObject/Tokens.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace BusinessObject
 {
     public class Tokens
     {
+        [Key]
         public string Token { get; set; }
         public string RefreshToken { get; set; }
 
